02a106b463375758e926d576ef5b2914fb62e644
andrew <sergienkoandrew9922@gmail.com>
2024-09-02 16:44:49 +0300

    refactor: edit database.sh

--- scripts/database.sh
+++ scripts/database.sh
@@ -3,15 +3,11 @@
 CERTS_DIR=/certs
 
 echo "Creating certificates..."
-if [[ -f $CERTS_DIR/ca.crt && -f $CERTS_DIR/ca.key && -f $CERTS_DIR/client.root.crt && -f $CERTS_DIR/client.root.key && -f $CERTS_DIR/node.crt && -f $CERTS_DIR/node.key ]]; then
-    echo "Certificates already exist"
-else
-    echo "Certificates do not exist, creating..."
-    rm -rf $CERTS_DIR/*
+rm -rf $CERTS_DIR/*
 
-    cockroach cert create-ca --certs-dir=$CERTS_DIR --ca-key=$CERTS_DIR/ca.key
-    cockroach cert create-node cockroachdb1 cockroachdb2 cockroachdb3 localhost 127.0.0.1 --certs-dir=$CERTS_DIR --ca-key=$CERTS_DIR/ca.key
-    cockroach cert create-client root --certs-dir=$CERTS_DIR --ca-key=$CERTS_DIR/ca.key
+cockroach cert create-ca --certs-dir=$CERTS_DIR --ca-key=$CERTS_DIR/ca.key
+cockroach cert create-node cockroachdb1 cockroachdb2 cockroachdb3 localhost 127.0.0.1 --certs-dir=$CERTS_DIR --ca-key=$CERTS_DIR/ca.key
+cockroach cert create-client root --certs-dir=$CERTS_DIR --ca-key=$CERTS_DIR/ca.key
 
-    chown -R 600 $CERTS_DIR
-fi
+chown -R 600 $CERTS_DIR
+
