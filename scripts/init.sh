de87bf918e694902aea5de61758260d686c5b5cb
andrew <sergienkoandrew9922@gmail.com>
2024-09-02 16:41:12 +0300

    refactor: edit init.sh

--- scripts/init.sh
+++ scripts/init.sh
@@ -23,12 +23,12 @@ echo "Initializing cluster..."
 docker exec cockroachdb1 cockroach init --certs-dir=$CERTS_DIR --host=localhost:26257
 
 echo "Creating database user..."
-docker exec cockroachdb1 cockroach sql --certs-dir=$CERTS_DIR --host=localhost:26257 --execute "CREATE USER $COCKROACH_USER WITH PASSWORD '$COCKROACH_PASSWORD';"
+docker exec cockroachdb1 cockroach sql --certs-dir=$CERTS_DIR --host=cockroachdb1:26257 --execute "CREATE USER $COCKROACH_USER WITH PASSWORD '$COCKROACH_PASSWORD';"
 docker exec cockroachdb1 cockroach cert create-client $COCKROACH_USER --certs-dir=$CERTS_DIR --ca-key=$CERTS_DIR/ca.key
 
 echo "Creating database..."
-docker exec cockroachdb1 cockroach sql --certs-dir=$CERTS_DIR --host=localhost:26257 --execute "CREATE DATABASE $COCKROACH_DATABASE;"
-docker exec cockroachdb1 cockroach sql --certs-dir=$CERTS_DIR --host=localhost:26257 --execute "GRANT ALL ON DATABASE $COCKROACH_DATABASE TO $COCKROACH_USER;"
+docker exec cockroachdb1 cockroach sql --certs-dir=$CERTS_DIR --host=cockroachdb1:26257 --execute "CREATE DATABASE $COCKROACH_DATABASE;"
+docker exec cockroachdb1 cockroach sql --certs-dir=$CERTS_DIR --host=cockroachdb1:26257 --execute "GRANT ALL ON DATABASE $COCKROACH_DATABASE TO $COCKROACH_USER;"
 
 echo "Database setup complete"
 docker compose -f ./docker-compose.yml --profile main up
